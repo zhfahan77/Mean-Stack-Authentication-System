1f4385be0f9227cf53eb3304778a57e9b8079056
Zamiul Hasan <zhfahan77@gmail.com>
2017-07-19 06:30:08 +0600

    Removed Hash Bang From URL

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var cookieParser = require("cookie-parser");
 require('./api/config/passport.js');
 
 // Middlewere to log request
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     console.log(req.method, req.url);
     next();
 });
@@ -20,8 +20,10 @@ app.use(passport.session());
 app.use(cookieParser());
 
 // parse application/x-www-form-urlencoded 
-app.use(bodyParser.urlencoded({ extended: false }));
- 
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 // parse application/json 
 app.use(bodyParser.json());
 
@@ -30,6 +32,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // api routes
 var apiroutes = require("./api/routes");
+app.use(function apiChecker(req, res, next) {
+    console.log('/// ROUTER RUNNING ///'); // will print on every route
+    console.log('URL:', req.originalUrl); // will show requested URL
+    var regex = /(\/api\/)/g; // pattern which checks for `/api/` in the URL
+    if (regex.test(req.originalUrl)) { // if the URL contains the pattern, then `next()`
+        console.log('THIS IS AN API REQUEST'); // api detected
+        next();
+    } else { // if the URL does not contain `/api`:
+        res.sendFile(path.join(__dirname, 'public/index.html')); // delivers index.html which angular-route will then load appropriate partial
+    }
+})
 app.use('/api', apiroutes);
 
 // declaring ports
