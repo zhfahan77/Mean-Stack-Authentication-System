234e9af4462c42520691ecd73a6991c234213ba2
Zamiul Hasan <zhfahan77@gmail.com>
2017-07-19 07:38:30 +0600

    dynamic page title added

--- public/app/main/app.client.js
+++ public/app/main/app.client.js
@@ -7,31 +7,35 @@ function config($routeProvider, $httpProvider, $locationProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'app/main/main.html',
-            controllerAs: 'vm'
+            controllerAs: 'vm',
+            title: 'Home Page'
         })
         .when('/private', {
             templateUrl: 'app/private/private.html',
             controller: 'privateController',
             controllerAs: 'vm',
-            needLogin: true
+            needLogin: true,
+            title: 'Private'
         })
         .when('/login', {
             templateUrl: 'app/login/login.html',
             controller: 'loginController',
             controllerAs: 'vm',
-            ifLogged: true
+            ifLogged: true,
+            title: 'Login'
         })
         .when('/register', {
             templateUrl: 'app/register/register.html',
             controller: "registerController",
             controllerAs: 'vm',
-            ifLogged: true
+            ifLogged: true,
+            title: 'Register'
         })
         .otherwise({
             redirectTo: '/404',
             templateUrl: 'app/main/404.html'
         });
-    
+
     $locationProvider.html5Mode(true);
     $locationProvider.hashPrefix('');
 }
@@ -42,10 +46,18 @@ function run($rootScope, $location, authentication) {
             $location.path('/login');
         }
     });
-    
+
     $rootScope.$on('$routeChangeStart', function (event, nextRoute, currentRoute) {
         if (authentication.isLoggedIn() && nextRoute.ifLogged) {
             $location.path('/private');
+        }
+    });
+
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+
+        if (current.hasOwnProperty('$$route')) {
+
+            $rootScope.title = current.$$route.title;
         }
     });
 }
